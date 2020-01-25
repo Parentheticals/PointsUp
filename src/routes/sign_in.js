951b0dfb559e1df94da4426b3eaa81c047f19c95
Parentheticals <davidwwangwang@gmail.com>
2020-01-25 05:29:23 -0600

    initial commit

--- src/routes/sign_in.js
+++ src/routes/sign_in.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let StudentModel = require('../models/person');
 let path = require('path');
 let express = require('express');
@@ -31,38 +30,4 @@ router.get('/sign_in_form',(req,res)=>{
     return res.render('signin');
 })
 
-=======
-let StudentModel = require('../models/person');
-let path = require('path');
-let express = require('express');
-let router = express.Router();
-
-// logemail and logpass is being passed from the form in sign_in
-router.post('/sign_in',(req,res,next)=>{
-    StudentModel.authentication(req.body.logemail, req.body.logpass, (err,user)=>{
-        //debug
-        // console.log(`Get ${req.body.logemail} and ${req.body.logpass}`);
-
-        if(err || !user ){ //weird stuff that is not supposed to happen
-            let err = new Error("Wrong email or password");
-            err.status = 401;
-            return next(err);
-        } else {
-            req.session.userId = user._id;
-            // Update this
-            // Check if the user has joined an organization
-            // if(user.organization.length==0){ // User has not joined an organization
-            //     return res.redirect(`/organizations/${user._id}`);
-            // }
-            return res.redirect(`/${user._id}/profile`);
-        }
-    })
-})
-
-// Go to sign_in html file
-router.get('/to_sign_in',(req,res)=>{
-    return res.sendFile(path.join(__dirname,"../../public/static/sign_in.html"))
-})
-
->>>>>>> a00f69ecf1071a6e080d8a1169d22c84d87f4aa7
 module.exports = router;
--- src/routes/sign_out.js
+++ src/routes/sign_out.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let express = require('express');
 let path = require('path');
 let router = express.Router();
@@ -17,24 +16,4 @@ router.get('/sign_out',(req,res,next)=>{
     }
 })
 
-=======
-let express = require('express');
-let path = require('path');
-let router = express.Router();
-
-router.get('/sign_out',(req,res,next)=>{
-    // console.log(`I am here save ${req.session}`)
-    if(req.session){
-        req.session.destroy(err=>{
-            if(err){
-                return next(err);
-            } else {
-                // console.log(`Am I here?`)
-                return res.redirect('/');
-            }
-        })
-    }
-})
-
->>>>>>> a00f69ecf1071a6e080d8a1169d22c84d87f4aa7
 module.exports = router;
